653f0adc35e84faf8bde4f3378c2e7c17bc1afa2
bai <286104883@qq.com>
2017-12-05 16:34:09 +0800

    testCommit1

---
+++ src/aaa/aaaaa.java
@@ -0,0 +1,14 @@
+package aaa;
+
+public class aaaaa {
+
+	public static void main(String[] args) {
+		 String line = "01 B3 D2 01 33 00 00 00 47 04 47 0D 73 00 2D 3D ";
+		 String liane = "7D 54 0F 02 33 00 00 00 47 04 47 0D 71 00 6C 4E";
+		 
+		 String raw = line.replace(" ","");
+		 String raw1 = liane.replace(" ","");
+		 System.out.println(raw);
+		 System.out.println(raw1);
+	}
+}
